5cd693f80571edfe4f5b1687e60ac0103a8c4901
Bruno Lange <blangeram@gmail.com>
2019-09-07 19:08:46 -0700

    Ensures immutability for kwargs used in memo key serialization

--- exos.py
+++ exos.py
@@ -121,9 +121,21 @@ def flip(fn):
 
 class hashabledict(dict):
     """
-    Hashable if you can guarantee immutability.
-    Proceed with caution.
+    A hashable dicitionary that comes with the cost of immutability.
+    Any attempts to modify it after its inception yields a RunTimeError
     """
+    def __readonly__(self, *args, **kwargs):
+        raise RuntimeError('Cannot modify hashabledict')
+
+    __setitem__ = __readonly__
+    __delitem__ = __readonly__
+    pop = __readonly__
+    popitem = __readonly__
+    clear = __readonly__
+    update = __readonly__
+    setdefault = __readonly__
+    del __readonly__
+
     def __key(self):
         return tuple((k,self[k]) for k in sorted(self))
     def __hash__(self):
