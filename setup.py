4bcef6c8fed88870db41c89cb31d4b5bdde5ac97
Bruno Lange <blangeram@gmail.com>
2019-11-04 21:34:36 -0800

    Registers version 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ __license__ = 'MIT'
 
 setup(
     name='exos',
-    version='0.1.1',
+    version='0.2',
     packages=['exos',],
     description='EXpressions Over Statements: additional functional tools in Python!',
     author='Bruno Lange',
