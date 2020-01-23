1f3aa48a4bf3cb09aea76efa211bde797fdfb83d
Bruno Lange <blangeram@gmail.com>
2020-01-22 22:14:31 -0800

    Adds another test for reduce_right

--- test/test_reduce_right.py
+++ test/test_reduce_right.py
@@ -7,3 +7,14 @@ class TestReduceRight(unittest.TestCase):
         sum1to100 = reduce(lambda acc, curr: acc+curr, range(1, 101), 0)
         sum100to1 = reduce_right(lambda curr, acc: acc+curr, range(1, 101), 0)
         self.assertTrue(sum1to100 == sum100to1 == 5050)
+
+    def test_reversal(self):
+        text = 'Hello, World'
+        reverse = reduce_right(lambda curr, acc: acc + curr, text, '')
+        self.assertTrue(reverse == text[::-1])
+
+        functions = ['f', 'g', 'h']
+        self.assertEqual(
+            reduce_right(lambda curr, acc: '{}({})'.format(curr, acc), functions, 'x'),
+            'f(g(h(x)))'
+        )
