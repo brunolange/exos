4b5c04a824cc3b3618f6556cd58b68baf1c52ea1
Bruno Lange <blangeram@gmail.com>
2020-05-27 22:44:32 -0700

    Simple implementation of a Maybe monad

---
+++ exos/monads/maybe.py
@@ -0,0 +1,38 @@
+"""Simple implementation of a Maybe monad
+"""
+
+__author__ = 'Bruno Lange'
+__email__ = 'blangeram@gmail.com'
+__license__ = 'MIT'
+
+
+class Maybe:
+    pass
+
+
+class Just(Maybe):
+    def __init__(self, value):
+        self.value = value
+
+
+    def bind(self, fn):
+        return Just(fn(self.value))
+
+
+    def __eq__(self, other):
+        return isinstance(other, Just) and other.value == self.value
+
+
+    def __str__(self):
+        return f'Just {self.value}'
+
+
+class _Nothing(Maybe):
+    def bind(self, _):
+        return self
+
+
+    def __str__(self):
+        return f'Nothing'
+
+Nothing = _Nothing()
---
+++ test/test_maybe.py
@@ -0,0 +1,17 @@
+import unittest
+
+from exos.monads.maybe import Maybe, Just, Nothing
+
+class TestMaybe(unittest.TestCase):
+    def test_nothing(self):
+        n = Nothing
+        self.assertTrue(isinstance(n, Maybe))
+
+    def test_just(self):
+        n = Just(42)
+        self.assertTrue(isinstance(n, Maybe))
+        self.assertEquals(n.value, 42)
+
+    def test_bind(self):
+        self.assertEqual(Just(6).bind(lambda x: x*7), Just(42))
+        self.assertEqual(Nothing.bind(lambda x: x*7), Nothing)
